4a87305a206f51433e3cfce11b95f9eb83cf295d
Candido Souza <candidosouzza@gmail.com>
2021-11-29 08:07:02 -0300

    :lipstick: Estilizando os body da tabela

--- src/components/Table/Body.tsx
+++ src/components/Table/Body.tsx
@@ -1,10 +1,10 @@
 const Body = (props) => {
     return props.clients?.map((client, i) => {
         return (
-            <tr key={client.id}>
-                <td>{client.id}</td>
-                <td>{client.firstName}</td>
-                <td>{client.age}</td>
+            <tr key={client.id} className={`${i % 2 === 0 ? 'bg-purple-100' : 'bg-purple-50'}`}>
+                <td className="text-left p-4">{client.firstName}</td>
+                <td className="text-left p-4">{client.id}</td>
+                <td className="text-left p-4">{client.age}</td>
             </tr>
         )
     });

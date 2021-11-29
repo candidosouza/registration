42baa2cdd89705f34a59a93d26036343d53b3614
Candido Souza <candidosouzza@gmail.com>
2021-11-29 08:07:26 -0300

    :lipstick: Estilizando o head da tabela

--- src/components/Table/Head.tsx
+++ src/components/Table/Head.tsx
@@ -1,9 +1,12 @@
 const Head = () => {
     return (
-        <tr>
-            <th>Código</th>
-            <th>Nome</th>
-            <th>Idade</th>
+        <tr className={`
+            text-gray-100
+            bg-gradient-to-r from-purple-500 to-purple-800
+        `}>
+            <th className="text-left p-4">Código</th>
+            <th className="text-left p-4">Nome</th>
+            <th className="text-left p-4">Idade</th>
         </tr>
     )
 }
